ff4dc4e01c6a2cecc103ee8779c24301796ad95f
heeeopen <ddablue@inu.ac.kr>
2025-05-11 21:16:52 +0900

    Feat: 최종 노선도 구현, 화면 전환

---
+++ android/app/src/main/kotlin/com/rushcutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rushcutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screen/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:rushcutter/screen/congestion_prediction_screen.dart';
@@ -9,6 +10,13 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env"); // .env 파일 로드
 
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white, // 상태바 배경 흰색
+      statusBarIconBrightness: Brightness.dark, // 안드로이드: 아이콘 검정
+      statusBarBrightness: Brightness.light,    // iOS: 아이콘 검정
+    ),
+  );
   runApp(
     MultiProvider(
       providers: [
@@ -26,7 +34,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rushcutter',
-      theme: ThemeData(primarySwatch: Colors.blue),
+        theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        ),
       home: const HomeScreen(),
       routes: {
         '/congestion': (context) {
